6a10150ceff8b68e7711c4e82b3eefbffd18b18b
Mert Aydın <mcanaydin60@trakya.edu.tr>
2026-08-17 19:36:48 +0300

    DbContext’i API’ye kaydet.
    Bağlantı string yoksa erken patlasın; CRUD endpoint’i hâlâ yok.

--- src/StudyTracker.Api/Program.cs
+++ src/StudyTracker.Api/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using StudyTracker.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+var connectionString = builder.Configuration.GetConnectionString("StudyTracker")
+    ?? throw new InvalidOperationException("Connection string 'StudyTracker' is missing.");
+
+builder.Services.AddDbContext<StudyTrackerDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
