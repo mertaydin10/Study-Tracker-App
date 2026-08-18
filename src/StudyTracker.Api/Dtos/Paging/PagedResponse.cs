218faf77d7165be7224cc78c17eb5d3475c86e3c
Mert Aydın <mcanaydin60@trakya.edu.tr>
2026-08-19 00:20:54 +0300

    Sayfalı liste DTO’sunu ekle.
    Toplam kayıt sayısı olmadan son sayfa bilinmez.

---
+++ src/StudyTracker.Api/Dtos/Paging/PagedResponse.cs
@@ -0,0 +1,9 @@
+namespace StudyTracker.Api.Dtos.Paging;
+
+public sealed class PagedResponse<T>
+{
+    public required IReadOnlyList<T> Items { get; init; }
+    public int Page { get; init; }
+    public int PageSize { get; init; }
+    public int TotalCount { get; init; }
+}
