d6929195580e4ab880711d7247c80bef67ac6fe5
Mert Aydın <mcanaydin60@trakya.edu.tr>
2026-08-19 18:48:31 +0300

    Program.cs’i yalnızca başlatma sırasına indir.
    Servis ve middleware uzantılardan çağrılsın.

--- src/StudyTracker.Api/Program.cs
+++ src/StudyTracker.Api/Program.cs
@@ -1,58 +1,8 @@
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using StudyTracker.Api.Data;
-using StudyTracker.Api.Entities;
+using StudyTracker.Api.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddOpenApi();
-builder.Services.AddSingleton<PasswordHasher<User>>();
-
-var connectionString = builder.Configuration.GetConnectionString("StudyTracker")
-    ?? throw new InvalidOperationException("Connection string 'StudyTracker' is missing.");
-
-builder.Services.AddDbContext<StudyTrackerDbContext>(options =>
-    options.UseNpgsql(connectionString));
-
-var jwtKey = builder.Configuration["Jwt:Key"]
-    ?? throw new InvalidOperationException("Jwt:Key is missing.");
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
-        };
-    });
-builder.Services.AddAuthorization();
+builder.Services.AddStudyTrackerApi(builder.Configuration);
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/openapi/v1.json", "Study Tracker");
-    });
-}
-
-app.UseHttpsRedirection();
-app.UseDefaultFiles();
-app.UseStaticFiles();
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapControllers();
-
+app.UseStudyTrackerApi();
 app.Run();
