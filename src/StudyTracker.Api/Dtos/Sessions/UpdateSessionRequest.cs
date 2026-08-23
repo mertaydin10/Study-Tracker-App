9f44f59b90e41c52e143b104bc39dd31650eead4
Mert Aydın <mcanaydin60@trakya.edu.tr>
2026-08-23 20:04:56 +0300

    Oturum güncellemesinde süre üst sınırını uygula.

--- src/StudyTracker.Api/Dtos/Sessions/UpdateSessionRequest.cs
+++ src/StudyTracker.Api/Dtos/Sessions/UpdateSessionRequest.cs
@@ -10,7 +10,7 @@ public sealed class UpdateSessionRequest
     [Required]
     public DateTimeOffset StartedAt { get; set; }
 
-    [Range(1, int.MaxValue)]
+    [Range(1, 1440)]
     public int DurationMinutes { get; set; }
 
     [MaxLength(2000)]
