f19e0be47fd0e81eb06ecc6c1b15158370ec134f
Mert Aydın <mcanaydin60@trakya.edu.tr>
2026-08-24 20:03:03 +0300

    Konu güncelleme doğrulamasını Türkçeleştir.

--- src/StudyTracker.Api/Dtos/Subjects/UpdateSubjectRequest.cs
+++ src/StudyTracker.Api/Dtos/Subjects/UpdateSubjectRequest.cs
@@ -4,8 +4,8 @@ namespace StudyTracker.Api.Dtos.Subjects;
 
 public sealed class UpdateSubjectRequest
 {
-    [Required]
-    [MinLength(1)]
-    [MaxLength(200)]
+    [Required(ErrorMessage = "Konu adı gerekli.")]
+    [MinLength(1, ErrorMessage = "Konu adı gerekli.")]
+    [MaxLength(200, ErrorMessage = "Konu adı en fazla 200 karakter olabilir.")]
     public string Name { get; set; } = "";
 }
